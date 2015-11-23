f285352e87383616efc8ac02e78485fac789ad21
Peter Vijeh <pvijeh@gmail.com>
2015-11-23 15:23:59 -0500

    removing footer and feeback component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,6 @@ import styles from './App.scss';
 import withContext from '../../decorators/withContext';
 import withStyles from '../../decorators/withStyles';
 import Header from '../Header';
-import Feedback from '../Feedback';
-import Footer from '../Footer';
 
 @withContext
 @withStyles(styles)
@@ -22,8 +20,6 @@ class App extends Component {
       <div>
         <Header />
         {this.props.children}
-        <Feedback />
-        <Footer />
       </div>
     ) : this.props.children;
   }
